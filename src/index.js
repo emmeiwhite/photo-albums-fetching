a0ef0e9e1e22249c2c5c091d85ffb63d0f7dbcf2
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-17 18:39:12 +0530

    Store and Redux setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const el = document.querySelector("#root");
 
 const root = createRoot(el);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
